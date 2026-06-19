6ff3f1913eacffb12410d48b7d7b56d81a3912b1
naman47vyas <writetonv@tutamail.com>
2026-06-19 14:12:23 +0530

    flakes

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -112,6 +115,9 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
+  
+
 
   # List services that you want to enable:
 
